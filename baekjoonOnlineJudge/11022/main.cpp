4e5248ef2d4e9bc96d77fab719c66e1582633288
sdkcoding <deter364@gmail.com>
2019-03-24 13:39:57 +0900

    11022

---
+++ baekjoonOnlineJudge/11022/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c;
+
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		cin >> b >> c;
+		cout << "Case #" << i + 1 << ": " << b << " + " << c << " = "<< b + c << '\n';
+	}
+
+	return 0;
+}
