862c7979de553da05dd72af161f7d551f58d97cc
sdkcoding <deter364@gmail.com>
2019-03-24 14:00:33 +0900

    7287

---
+++ baekjoonOnlineJudge/7287/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+
+	cout << 15 << '\n';
+	cout << "deter9" << '\n';
+
+	return 0;
+}
